ea5567f44bd2d9378e57f262505fb1d70f64bf43
almatairi <almtahirii@gmail.com>
2024-01-25 15:31:41 +0100

    added models to the admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+class FoodInLine(admin.StackedInline):
+    model = models.Food
+    extra = 1
+
+
+class FoodAdmin(admin.ModelAdmin):
+    exclude = ["author"]
+
+    def save_model(self, request, obj, form, change):
+        if obj is not None:
+            obj.author = request.user
+
+        super().save_model(request, obj, form, change)
+
+    def has_change_permission(self, request, obj=None):
+        return obj is not None and obj.author == request.user
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [FoodInLine]
+    list_display = ["name"]
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ["name", "surname"]
+
+
+admin.site.register(models.Food, FoodAdmin)
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Client, ClientAdmin)
+admin.site.register(models.Sale)
+admin.site.register(models.ProductSale)
+
